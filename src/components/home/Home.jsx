887f643144c13a315ab800b75f94ccadf9f87d5f
Stanislav Huseinov <stashuseinov@gmail.com>
2023-03-25 13:57:09 +0200

    add transition group lib

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,59 +1,60 @@
-import React, { useState } from 'react'
-import TodoItem from './item/TodoItem'
-import CreateTodoField from './CreateTodoField'
+import React, { useState } from 'react';
+import TodoItem from './item/TodoItem';
+import CreateTodoField from './CreateTodoField';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const data = [
-    {
-        _id: "1",
-        title: "Do the homework",
-        isCompleted: false,
-    },
-    {
-        _id: "2",
-        title: "Go to work",
-        isCompleted: false,
-    },
-    {
-        _id: "3",
-        title: "Check my reddit account",
-        isCompleted: false,
-    },
-    {
-        _id: "4",
-        title: "Watch movie",
-        isCompleted: false,
-    }
-]
-
+  {
+    _id: '1',
+    title: 'Do the homework',
+    isCompleted: false,
+  },
+  {
+    _id: '2',
+    title: 'Go to work',
+    isCompleted: false,
+  },
+  {
+    _id: '3',
+    title: 'Check my reddit account',
+    isCompleted: false,
+  },
+  {
+    _id: '4',
+    title: 'Watch movie',
+    isCompleted: false,
+  },
+];
 
 const Home = () => {
+  const [todos, setTodos] = useState(data);
 
-    const [todos, setTodos] = useState(data)
+  const changeTodo = (id) => {
+    const copy = [...todos];
+    const current = copy.find((t) => t._id === id);
+    current.isCompleted = !current.isCompleted;
+    setTodos(copy);
+  };
 
-    const changeTodo = id => {
-        const copy = [...todos]
-        const current = copy.find(t => t._id === id)
-        current.isCompleted = !current.isCompleted
-        setTodos(copy);
-    }
+  const removeTodo = (id) => setTodos([...todos].filter((t) => t._id !== id));
 
+  return (
+    <div>
+      <h1 className="text-3xl text-center mb-3 mt-5 text-black">My to do list</h1>
+      <CreateTodoField setTodos={setTodos}></CreateTodoField>
+      {todos.length ? (
+        <TransitionGroup component={'div'}>
+          {todos.map((todo) => (
+          <CSSTransition key={todo._id} timeout={500} classNames="todo">
+            <TodoItem todo={todo} changeTodo={changeTodo} removeTodo={removeTodo} />
+          </CSSTransition>
+          ))}
+        </TransitionGroup>
+      ) : (
+        <div className="text-center text-2xl">Nothing To Do;(</div>
+      )}
+    </div>
+  );
+};
 
-    const removeTodo = id => setTodos([...todos].filter(t => t._id !== id))
-
-
-
-
-
-    return (
-        <div >
-            <h1 className="text-3xl text-center mb-3 mt-5 text-black">My to do list</h1>
-            <CreateTodoField setTodos={setTodos}></CreateTodoField>
-            {todos.length ? todos.map(todo => (
-                <TodoItem todo={todo} key={todo._id} changeTodo={changeTodo} removeTodo={removeTodo} />
-            ))
-                : <div className='text-center text-2xl'>To do отсутствуют</div>}
-        </div>
-    )
-}
-
-export default Home
+export default Home;
